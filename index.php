8997444ee6c50c09d580b6ec4d9bf10d9211934c
IITS\se20iit <ivo5055@abv.bg>
2023-11-02 12:50:49 +0200

    no message

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        Здравей :)
+    </body>
+</html>
+
+<?php
+
+$target = "contacts.php";
+$linkname = "mylink";
+link($target, $linkname);
+
+?>
